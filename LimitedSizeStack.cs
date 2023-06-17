a334a6804632cb3d9a591e8559aac35f753bc200
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-06-17 23:05:49 +0500

    Добавьте файлы проекта.

---
+++ LimitedSizeStack.cs
@@ -0,0 +1,71 @@
+﻿// Вставьте сюда финальное содержимое файла LimitedSizeStack.cs
+using System;
+
+namespace LimitedSizeStack;
+
+public class LimitedSizeStack<T>
+{
+    private Element<T> head { get; set; }
+    private Element<T> tail { get; set; }
+    int length { get; set; }
+
+    public LimitedSizeStack(int undoLimit)
+    {
+        length = undoLimit;
+    }
+
+    public void Push(T item)
+    {
+        if (head == null && tail == null)
+        {
+            if (length > 0)
+            {
+                head = tail = new Element<T> { Value = item };
+                Count++;
+            }
+        }
+
+        else
+        {
+            if (length == Count)
+            {
+                head = head.Next;
+                if (head !=null)  head.Prev = null;
+                Count--;
+            }
+
+            var now = new Element<T> { Value = item, Prev = tail };
+            tail.Next = now;
+            tail = now;
+            Count++;
+        }
+    }
+
+    public T Pop()
+    {
+        if (Count == 0 || length == 0) throw new Exception();
+        var result = tail.Value;
+        if (Count == 1)
+        {
+            head = null;
+            tail = null;
+            Count--;
+        }
+
+        else
+        {
+            tail = tail.Prev;
+            Count--;
+        }
+
+        return result;
+    }
+
+    public int Count { get; private set; }
+}
+public class Element<T>
+{
+    public T Value;
+    public Element<T> Next;
+    public Element<T> Prev;
+}
---
+++ ListModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class ListModel<TItem>
+{
+    public List<TItem> Items { get; }
+    public int UndoLimit;
+    public LimitedSizeStack<Dictionary<int, TItem>> Operations;
+
+    public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
+    {
+        UndoLimit = undoLimit;
+        Operations = new LimitedSizeStack<Dictionary<int, TItem>>(undoLimit);
+    }
+
+    public ListModel(List<TItem> items, int undoLimit)
+    {
+        Items = items;
+        UndoLimit = undoLimit;
+        Operations = new LimitedSizeStack<Dictionary<int, TItem>>(undoLimit);
+    }
+
+    
+
+    public void AddItem(TItem item)
+    {
+        Items.Add(item);
+        Operations.Push(new Dictionary<int, TItem> { { -1, item } });
+    }
+
+    public void RemoveItem(int index)
+    {
+        Operations.Push(new Dictionary<int, TItem> { { index, Items[index] } });
+        Items.RemoveAt(index);
+    }
+
+    public bool CanUndo()
+    {
+        return Operations!= null && Operations.Count != 0 ;
+    }
+
+    public void Undo()
+    {
+        //throw new NotImplementedException();
+        var doo = Operations.Pop();
+        foreach (var kee in doo.Keys)
+        {
+            if (kee < 0) Items.RemoveAt(Items.Count - 1);
+            else Items.Insert(kee, doo[kee]);
+        }
+    }
+}
